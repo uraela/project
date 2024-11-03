010f36081a4cb7dccc716e363cce68f2fd8afe4c
uraela <pexelsaur@gmail.com>
2024-11-02 23:35:14 -0700

    latest working

--- pages/2_Methodology.py
+++ pages/2_Methodology.py
@@ -4,4 +4,4 @@ st.title ("Methodology")
 st.html("<p style='padding-top:15px'> </p>")
 
 filepath = './data/methodology.png'
-st.image(filepath, caption="methodology")
+st.image(filepath, caption="*  https://data.gov.sg/datasets?agencies=SkillsFuture+Singapore+(SSG)&page=1&resultId=d_563451336616abdf5b2c36472c2afd8b")
